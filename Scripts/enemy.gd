38a1bce5c84921ba4be971df2636c3c20b6548b5
Szyrrogi <szyrrogi@hotmail.com>
2026-02-28 18:56:05 +0100

    intent

--- Scripts/enemy.gd
+++ Scripts/enemy.gd
@@ -1,8 +1,13 @@
 extends Character
 
 @export var player: Node2D
+@export var status_text: RichTextLabel
+@export var status_art: Sprite2D
+
 const CardDatabase = preload("res://Scripts/CardDatabase.gd")
 var deck_data = [1, 2]
+var effect_next
+var effect
 
 func action():
 	if deck_data.is_empty():
@@ -10,17 +15,22 @@ func action():
 		return
 		
 	deck_data.shuffle() 
-	var card_id = deck_data.pop_front() 
+	var card_id = deck_data[0]
 
 	var card_data = CardDatabase.CARDS[card_id] # Pamiętaj o dobrej ścieżce/nazwie Singletona Bazy
 	var card_name = card_data[0]
 	var put_type = card_data[5]
-	var effect_data = card_data[6]
+	effect_next = card_data[6]
+	
+	if(effect == null):
+		effect = effect_next
+		set_status()
+		pass
 	
 	print("Przeciwnik zagrywa kartę: ", card_name)
 
 	var virtual_card = {
-		"effect": effect_data
+		"effect": effect
 	}
 
 	if put_type == 0:
@@ -30,6 +40,11 @@ func action():
 	elif put_type == 1:
 		print("Przeciwnik używa karty na sobie!")
 		self.take(virtual_card)
+	set_status()
+	
+func set_status():
+	status_text.text = "[font_size=155]" + str(effect_next[1])
+	status_art.texture = load("res://Art/Stats/Status" + str(effect_next[0]) + ".png")
 
 func die():
 	print("Przeciwnik pokonany!")
